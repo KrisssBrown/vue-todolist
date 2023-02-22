201c7dd45226898964f1cb6aa9de1ffa622f81ed
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-22 01:24:03 +0100

    ADD toggle checked tasks (to fix)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,67 @@
 const { createApp } = Vue
 
+
+let tasks = [
+    {
+        text: 'Quarterly Newsletter',
+        done: false
+    }, {
+        text: 'Recruiting blog post',
+        done: false
+    }, {
+        text: 'Mobile app launch',
+        done: false
+    }, {
+        text: 'Interview Jhon H.',
+        done: false
+    }, {
+        text: 'Summit update to mobile storefronts',
+        done: false
+    }, {
+        text: 'Schedule Meeting with Alex',
+        done: false
+    }, {
+        text: 'Homepage refresh',
+        done: false
+    }, {
+        text: 'Onboard new Sales team members',
+        done: false
+    }, {
+        text: 'Review editorial calendar ',
+        done: false
+    }
+]
+
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            tasks: tasks,
+            inputValue: '',
+            isActive: false
+
+        }
+    },
+
+    methods: {
+        addTask() {
+            // console.log(this.inputValue)
+            const newTask = this.inputValue
+            console.log(newTask)
+            this.tasks.push(newTask)
+            this.inputValue = ''
+            console.log(this.inputValue)
+            console.log(this.tasks)
+        },
+        
+        removeTask(startIndex){
+            this.tasks.splice(startIndex, 1)
+        },
+        toggleActive() {
+            this.isActive = !this.isActive;
+            console.log(this.isActive)
+        },
+        updated(){
+            console.log(isChecked)
         }
     }
 }).mount('#app')
